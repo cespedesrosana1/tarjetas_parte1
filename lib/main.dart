87ca1ef9a844266ac1df1c1113fdd8cd2b8e17b5
S684256 <rosana.cespedes@evertecinc.com>
2022-09-10 07:44:18 -0300

    Agregar proyecto con diseño solo

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:tarjetas_parte1/pages/pago_completo_page.dart';
+
+import 'pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'StripeApp',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => HomePage(),
+        'pago_completo': ( _ ) => PagoCompletoPage(),
+      },
+      theme: ThemeData.light().copyWith(
+        primaryColor: Color(0xff284879),
+        scaffoldBackgroundColor: Color(0xff21232A)
+      ),
+    );
+  }
+}
+
